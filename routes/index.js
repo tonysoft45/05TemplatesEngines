a17835104c4408a53128115fe0f71b808cc9c9e5
Antonio Barajas <tonysoft2018@outlook.com>
2019-08-06 13:07:52 -0500

    cambios para manejo de errores

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,19 @@
 var express = require('express')
 var router = express.Router()
 
+function jade(req,res,next){
+    let locals= {
+        title : 'Jade'
+    }
+    res.render('index',locals)
+
+}
+
 router.get('/', function(req,res){
     res.end('<h1> Prueba de configuracion primer app en express </h1>')
 })
+
+router.get('/jade', jade)
 
 
 module.exports = router
